d852ffed4ad6d2051c259752c68d6c16c6ebc03e
Tyler Owen Morris <theowenmorris@gmail.com>
2025-04-27 13:04:48 -0700

    updating s3 deployment from cdk to hopefully just find the bucket, and build its own frontend to upload

--- infra/lib/frontend-stack.ts
+++ infra/lib/frontend-stack.ts
@@ -1,28 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class PirateTradeWarsFrontendStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const bucket = new s3.Bucket(this, 'PirateTradeWarsFrontendBucket', {
-            bucketName: 'piratetradewars-frontend-bucket',
-            websiteIndexDocument: 'index.html',
-            publicReadAccess: true,
-            blockPublicAccess: new s3.BlockPublicAccess({
-                blockPublicAcls: false,
-                ignorePublicAcls: false,
-                blockPublicPolicy: false,
-                restrictPublicBuckets: false,
-            }),
-        });
+        // Import existing bucket
+        const bucket = s3.Bucket.fromBucketName(this, 'PirateTradeWarsFrontendBucket', 'piratetradewars-frontend-bucket');
 
-        //  SKIPPED FOR NOW: the frontend is deployed to the bucket through the other build process, we don't need to do it here
-        // new s3deploy.BucketDeployment(this, 'PirateTradeWarsDeployFrontend', {
-        //     sources: [s3deploy.Source.asset('../dist/public')], // Adjust path to your React build directory
-        //     destinationBucket: bucket,
-        // });
+        // Debug: Print directory contents
+        console.log('Current directory (infra/lib):', fs.readdirSync(__dirname));
+        console.log('One level up (infra):', fs.readdirSync(path.join(__dirname, '..')));
+        console.log('Two levels up (repository root):', fs.readdirSync(path.join(__dirname, '../..')));
+        console.log('Checking existence of dist/public:', fs.existsSync(path.join(__dirname, '../../dist/public')));
+
+        new s3deploy.BucketDeployment(this, 'PirateTradeWarsDeployFrontend', {
+            sources: [s3deploy.Source.asset('../../dist/public')],
+            destinationBucket: bucket,
+        });
 
         new cdk.CfnOutput(this, 'PirateTradeWarsBucketUrl', {
             value: bucket.bucketWebsiteUrl,
