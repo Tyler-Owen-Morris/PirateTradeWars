cdcc98c3147f312a7269a12aecb07e7085c13a13
Tyler Owen Morris <theowenmorris@gmail.com>
2025-04-26 18:22:57 -0700

    ci/cd setup

---
+++ infra/lib/frontend-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+
+export class PirateTradeWarsFrontendStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const bucket = new s3.Bucket(this, 'PirateTradeWarsFrontendBucket', {
+            bucketName: 'piratetradewars-frontend-bucket',
+            websiteIndexDocument: 'index.html',
+            publicReadAccess: true,
+            blockPublicAccess: new s3.BlockPublicAccess({
+                blockPublicAcls: false,
+                ignorePublicAcls: false,
+                blockPublicPolicy: false,
+                restrictPublicBuckets: false,
+            }),
+        });
+
+        new s3deploy.BucketDeployment(this, 'PirateTradeWarsDeployFrontend', {
+            sources: [s3deploy.Source.asset('../build')], // Adjust path to your React build directory
+            destinationBucket: bucket,
+        });
+
+        new cdk.CfnOutput(this, 'PirateTradeWarsBucketUrl', {
+            value: bucket.bucketWebsiteUrl,
+        });
+    }
+}
---
+++ infra/lib/game-server-stack.ts
@@ -0,0 +1,83 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class PirateTradeWarsGameServerStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const vpc = new ec2.Vpc(this, 'PirateTradeWarsGameVpc', { maxAzs: 2 });
+
+        const alb = new elbv2.ApplicationLoadBalancer(this, 'PirateTradeWarsAlb', {
+            vpc,
+            internetFacing: true,
+        });
+
+        const listener = alb.addListener('PirateTradeWarsListener', {
+            port: 443,
+            protocol: elbv2.ApplicationProtocol.HTTPS,
+            certificates: [], // Add ARN of ACM certificate for piratetradewars.com
+        });
+
+        const targetGroup = new elbv2.ApplicationTargetGroup(this, 'PirateTradeWarsTargetGroup', {
+            vpc,
+            port: 443,
+            protocol: elbv2.ApplicationProtocol.HTTPS,
+            targetType: elbv2.TargetType.INSTANCE,
+            healthCheck: {
+                path: '/health',
+                protocol: elbv2.Protocol.HTTPS,
+            },
+        });
+
+        listener.addTargetGroups('PirateTradeWarsGameTarget', {
+            targetGroups: [targetGroup],
+            conditions: [
+                elbv2.ListenerCondition.pathPatterns(['/server-*']),
+            ],
+        });
+
+        const asg = new autoscaling.AutoScalingGroup(this, 'PirateTradeWarsGameAsg', {
+            vpc,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+            machineImage: ec2.MachineImage.latestAmazonLinux2(),
+            minCapacity: 0,
+            maxCapacity: 10,
+            desiredCapacity: 0,
+            userData: ec2.UserData.custom(`
+        #!/bin/bash
+        UPSTASH_REDIS_URL=$(aws secretsmanager get-secret-value --secret-id piratetradewars-upstash-redis-url --query SecretString --output text --region us-west-2)
+        echo "UPSTASH_REDIS_URL=$UPSTASH_REDIS_URL" >> /etc/environment
+        yum install -y nodejs
+        npm install -g pm2
+        cd /home/ec2-user
+        aws s3 cp s3://piratetradewars-deploy-bucket/backend.zip .
+        unzip backend.zip
+        npm install
+        pm2 start server/index.js
+      `),
+        });
+
+        asg.role.addManagedPolicy(
+            iam.ManagedPolicy.fromAwsManagedPolicyName('AWSSecretsManagerReadWrite')
+        );
+
+        targetGroup.addTarget(asg);
+
+        const coordinator = new ec2.Instance(this, 'PirateTradeWarsCoordinator', {
+            vpc,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+            machineImage: ec2.MachineImage.latestAmazonLinux2(),
+        });
+
+        coordinator.role.addManagedPolicy(
+            iam.ManagedPolicy.fromAwsManagedPolicyName('AutoScalingFullAccess')
+        );
+
+        new cdk.CfnOutput(this, 'PirateTradeWarsAlbDnsName', {
+            value: alb.loadBalancerDnsName,
+        });
+    }
+}
