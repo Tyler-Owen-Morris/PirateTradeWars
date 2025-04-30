215d0e99dee7f0fe8a6df328e5398644c335c755
Tyler Owen Morris <theowenmorris@gmail.com>
2025-04-30 10:22:45 -0700

    adding env vars to github for test run

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
     setupNodeEvents(on, config) {
-      // Optional: Add custom event listeners or tasks here
+      // Optional: Add custom event listeners or tasks here 
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}', // Pattern for test files
   }
